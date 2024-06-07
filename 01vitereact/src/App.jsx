8dc75d525ec59c4108e6905cccecc4c1c6a6e7fa
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-06-08 02:23:58 +0530

    custom react integrated

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,12 @@
 import Chai from "./chai.jsx"
 function App() {
+  const username = "rishabh777"
+  // variable are accessed using {} in which we only mention the final evaluated expression
   return (
+    <>
+    <h1>react JS for {username}</h1> 
     <Chai/>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp(){
+//     return (
+//         <div>
+//             <h1>Custom App</h1>
+//         </div>
+//     )
+// }
+
+// ReactElement is the representing of the parsed tree from HTML code
+// const ReactElement = {
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// }
+
+// const AnotherElement = (
+//     <a href="https://google.com">Click here to visit google</a>
+// )
+
+const anotherUser = 'pulto23'
+
+const ReactElement = React.createElement('a',{
+    href:'https://google.com',
+    target:'_blank'
+},'Click me to visit google',
+anotherUser
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+    // App is basically a function
     <App />
+    // MyApp()
+    // <MyApp/>
+    // ReactElement //problem is the fields are not same as the orginal documentation
+    // AnotherElement
+    // ReactElement
 )
